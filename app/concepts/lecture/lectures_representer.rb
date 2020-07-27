03af987a7b479324569702b086f22b8afb8ed71c
ArthurSiqueiraS <asesilva@inf.ufpel.edu.br>
2020-07-27 13:53:57 -0300

    Lecture conditional live

--- app/concepts/lecture/lectures_representer.rb
+++ app/concepts/lecture/lectures_representer.rb
@@ -1,6 +1,7 @@
 class LecturesRepresenter < CollectionRepresenter
   def self.decoration(lecture)
     lecture.as_json.merge({
+      live: lecture.live && Event.first.online,
       id: lecture.id.to_s,
       link: "/livestream/#{lecture.id.to_s}"
     })
